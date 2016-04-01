bb3132e395f0dce433f9cf9f185fdc46141ae6d2
leonsurwald <leonsurwald@Leons-MacBook-Pro.local>
2016-04-01 08:31:45 -0700

    update

--- src/crossyroad/MainCharacter.java
+++ src/crossyroad/MainCharacter.java
@@ -5,13 +5,59 @@
  */
 package crossyroad;
 
+import java.awt.Image;
+
 /**
  *
  * @author aidanmartin
  */
 public class MainCharacter {
-    
+
     //speed, direction, location, image, size
-    
+    private int speed;
+    private Direction direction;
+    private Image image;
+
+    /**
+     * @return the speed
+     */
+    public int getSpeed() {
+        return speed;
+    }
+
+    /**
+     * @param speed the speed to set
+     */
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    /**
+     * @return the direction
+     */
+    public Direction getDirection() {
+        return direction;
+    }
+
+    /**
+     * @param direction the direction to set
+     */
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    /**
+     * @return the image
+     */
+    public Image getImage() {
+        return image;
+    }
+
+    /**
+     * @param image the image to set
+     */
+    public void setImage(Image image) {
+        this.image = image;
+    }
     
 }
